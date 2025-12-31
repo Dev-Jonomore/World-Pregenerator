248487b1a0fe0d95ffa924b1d1d4661119c7e42a
Josiah Duvalian <jduvalian@harding.edu>
2025-12-30 20:12:41 -0600

    Initial setup

---
+++ src/main/java/mc/jonomore/worldPregenerator/WorldPregenerator.java
@@ -0,0 +1,17 @@
+package mc.jonomore.worldPregenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldPregenerator extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
