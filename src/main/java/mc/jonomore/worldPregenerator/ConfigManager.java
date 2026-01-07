b7e33a44cfa6474c2468ab7c9fd94aba9b5fad82
Josiah Duvalian <jduvalian@harding.edu>
2026-01-07 16:58:31 -0600

    Updated ConfigManager.java so that it caches config values for efficiency

--- src/main/java/mc/jonomore/worldPregenerator/ConfigManager.java
+++ src/main/java/mc/jonomore/worldPregenerator/ConfigManager.java
@@ -3,20 +3,32 @@ package mc.jonomore.worldPregenerator;
 public class ConfigManager {
   private final WorldPregenerator plugin;
 
+  // Cached config values
+  private int radius;
+  private String exportPath;
+  private String seedsFile;
+
   public ConfigManager(WorldPregenerator plugin) {
     this.plugin = plugin;
     plugin.saveDefaultConfig();
+    loadConfig();
+  }
+
+  public void loadConfig() {
+    radius = plugin.getConfig().getInt("radius");
+    exportPath = plugin.getConfig().getString("export-path");
+    seedsFile = plugin.getConfig().getString("seeds-file");
   }
 
   public int getRadius() {
-    return plugin.getConfig().getInt("chunk-radius");
+    return radius;
   }
 
   public String getExportPath() {
-    return plugin.getConfig().getString("export-path");
+    return exportPath;
   }
 
   public String getSeedsFile() {
-    return plugin.getConfig().getString("seeds-file");
+    return seedsFile;
   }
 }
