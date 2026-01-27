a114cd55598e2543b259868e7232876830c740f1
Josiah Duvalian <jduvalian@harding.edu>
2026-01-27 03:31:48 -0600

    Completed all commands

--- src/main/java/mc/jonomore/worldPregenerator/WPCommands.java
+++ src/main/java/mc/jonomore/worldPregenerator/WPCommands.java
@@ -2,12 +2,16 @@ package mc.jonomore.worldPregenerator;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.util.Tick;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WPCommands {
   private static boolean reset_window = false;
@@ -52,26 +56,98 @@ public class WPCommands {
             })
         )
         .then(Commands.literal("confirm")
+            .executes(ctx -> hardReset(ctx, wp))
+        )
+        .then(Commands.literal("reload")
             .executes(ctx -> {
-              if (reset_window) {
-                wp.stop();
-                // TODO: delete directories in Worlds folder and reset the task
-              } else {
-                ctx.getSource().getSender().sendMessage(
-                    MiniMessage.miniMessage().deserialize(
-                        "Reset window has expired or was never opened!"
-                    )
-                );
+              wp.config.loadConfig();
+              return Command.SINGLE_SUCCESS;
+            })
+        )
+        .then(Commands.literal("info")
+            .executes(ctx -> {
+              for (String line : wp.config.toString().split("\n")) {
+                wp.getLogger().info(line);
               }
               return Command.SINGLE_SUCCESS;
-            }))
-        .then(Commands.literal("status"))
-        .then(Commands.literal("reload"))
-        .then(Commands.literal("info"))
+            })
+        )
         .then(Commands.literal("config")
             .then(Commands.argument("setting", StringArgumentType.word())
-                .then(Commands.argument("value", StringArgumentType.word()))
+                .then(Commands.argument("value", StringArgumentType.word())
+                    .executes(ctx -> {
+                      String setting = StringArgumentType.getString(ctx, "setting");
+                      String value = StringArgumentType.getString(ctx, "value");
+                      wp.getConfig().set(setting, value);
+                      wp.saveConfig();
+                      wp.config.loadConfig();
+                      return Command.SINGLE_SUCCESS;
+                    })
+                )
             )
         ).build();
+  }
+
+  private static int hardReset(CommandContext<CommandSourceStack> ctx, WorldPregenerator wp) {
+    if (!reset_window) {
+      ctx.getSource().getSender().sendMessage(
+          MiniMessage.miniMessage().deserialize(
+              "Reset window has expired or was never opened!"
+          )
+      );
+      return Command.SINGLE_SUCCESS;
+    }
+
+    wp.stop();
+
+    File world_dir = new File(wp.config.getExportPath());
+    File[] worlds = world_dir.listFiles();
+
+    if (worlds == null) {
+      ctx.getSource().getSender().sendMessage(
+          MiniMessage.miniMessage().deserialize(
+              "<yellow>[WARNING] Failed to extract worlds from world directory."
+          )
+      );
+      return Command.SINGLE_SUCCESS;
+    }
+
+    if (worlds.length == 0) {
+      ctx.getSource().getSender().sendMessage("No worlds to delete!");
+      wp.task.reset();
+      return Command.SINGLE_SUCCESS;
+    }
+
+    ctx.getSource().getSender().sendMessage("Deleting " + worlds.length + " worlds...");
+    for (File world_folder : worlds) {
+      try {
+        util.deleteDirectory(world_folder);
+      } catch (IOException e) {
+        ctx.getSource().getSender().sendMessage(
+            MiniMessage.miniMessage().deserialize(
+                "<red>[ERROR] Failed to delete world folder. " + e.getMessage()
+            )
+        );
+      }
+    }
+
+    File[] remaining_worlds = world_dir.listFiles();
+    if (remaining_worlds != null && remaining_worlds.length != 0) {
+      ctx.getSource().getSender().sendMessage(
+          MiniMessage.miniMessage().deserialize(
+              "<yellow>[WARNING] there are still <dark_red>" +
+                  remaining_worlds.length + "</dark_red> worlds in your world directory."
+          )
+      );
+    } else {
+      ctx.getSource().getSender().sendMessage(
+          MiniMessage.miniMessage().deserialize(
+              "<green>All worlds successfully deleted!"
+          )
+      );
+    }
+
+    wp.task.reset();
+    return Command.SINGLE_SUCCESS;
   }
 }
